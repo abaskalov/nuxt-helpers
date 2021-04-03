3a62c6d14da610ed87468bd93e55bbad8ccb7307
aaa <abaskalov@me.com>
2021-04-03 17:05:34 +0600

    plugins

--- utils/ExtendVueApp.d.ts
+++
@@ -1,2 +0,0 @@
-import { NuxtAppOptions } from "@nuxt/types/app";
-export default function ExtendVueApp(app: NuxtAppOptions, mixin: any): void;
--- utils/ExtendVueApp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function ExtendVueApp(app, mixin) {
-    if (!app.mixins) {
-        app.mixins = [];
-    }
-    app.mixins.push(mixin);
-}
-exports.default = ExtendVueApp;
